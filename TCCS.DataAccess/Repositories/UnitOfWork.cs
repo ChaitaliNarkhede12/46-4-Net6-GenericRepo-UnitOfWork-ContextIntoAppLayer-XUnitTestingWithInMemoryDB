b7d9df42e62bd1108130d2297f3dbc8f1e826fc5
Harish Kolhe <chaitali_narkhede@epam.com>
2023-06-30 18:57:56 +0530

    chnages

---
+++ TCCS.DataAccess/Repositories/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCCS.DataAccess.Interfaces;
+using TCCS.DataAccess.Models;
+
+namespace TCCS.DataAccess.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; }
+
+        public UnitOfWork(TccsContext context)
+        {
+            Context = context;
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
